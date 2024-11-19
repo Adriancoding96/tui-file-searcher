7b8169547cb400ba95b6cbad0c0253d66069b732
Adriancoding96 <adrian.nilsson.coding@gmail.com>
2024-11-19 12:28:56 +0000

    ***Added*** function in app.rs to create a new application state, and a function in app.rs to save json key value pairs and reset application state after json is saved

--- src/app.rs
+++ src/app.rs
@@ -17,6 +17,9 @@ pub enum CurrentlyEditing {
     Value,
 }
 
+/*
+* This structure contains the full application state
+*/
 pub struct App {
     pub key_input: String,
     pub value_input: String,
@@ -24,3 +27,43 @@ pub struct App {
     pub current_screen: CurrentScreen,
     pub currently_editing: Option<CurrentlyEditing>,
 }
+
+/*
+* Function creates a new application state
+*
+* @return app: returns a app structs containing the application state
+*/
+impl App {
+    pub fn new() -> App {
+        App {
+            key_input: String::new(),
+            value_input: String::new(),
+            pairs: HashMap::new(),
+            current_screen: CurrentScreen::Main,
+            currently_editing: None,
+        }
+    }
+
+    /*
+    * Function called when user saves json key values, after saving
+    * it restores the application state;
+    *
+    * @param slef: contains a mutable reference to itslef
+    */
+    pub fn save_key_value(&mut self) {
+        self.pairs.insert(self.key_input.clone(), self.value_input.clone());
+        self.key_input = String::new();
+        self.value_input = String::new();
+        self.currently_editing = None;
+    } 
+
+}
+
+
+
+
+
+
+
+
+
