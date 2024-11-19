7f257e6d642f9323c63f2b0e368bf6145b6413ba
Adriancoding96 <adrian.nilsson.coding@gmail.com>
2024-11-19 12:13:58 +0000

    ***Added*** CurentlyEditing enum that defines the application states, App structs that contains the full application state

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,26 @@
+use std::collections::HashMap;
 
+/*
+* Enum defines the screens of the application
+*/
 pub enum CurrentScreen {
     Main,
     Editing,
     Exiting,
+}
+
+/*
+* Enum defines the states of the application
+*/
+pub enum CurrentlyEditing {
+    Key,
+    Value,
+}
+
+pub struct App {
+    pub key_input: String,
+    pub value_input: String,
+    pub pairs: HashMap<String, String>,
+    pub current_screen: CurrentScreen,
+    pub currently_editing: Option<CurrentlyEditing>,
 }
